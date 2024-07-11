6ebbab5aa054faecf3acd7a25e42ddbcf697f4ed
Jan Civitelli Benso <jan.civitelli@dolphin-it.de>
2024-07-11 10:52:27 +0200

    add submodule django_template_limitless

--- django_service_stack/settings.py
+++ django_service_stack/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Template
+    "django_template_limitless.django_template_limitless",
 ]
 # Dynamic loading of MDAT modules
 for name in os.listdir(PROJECT_ROOT + "/.."):
@@ -127,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
